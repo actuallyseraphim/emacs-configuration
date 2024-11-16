cfdc6c634c8f5822fb995df5b49d417c77c5364f
actuallyseraphim <actuallyseraphim@gmail.com>
2024-11-16 14:13:28 +0000

    add config

---
+++ elpa/math-preview-20240801.513/math-preview-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "math-preview" "20240801.513"
+  "Preview TeX math equations inline."
+  '((emacs "26.1")
+    (json  "1.4")
+    (dash  "2.18.0")
+    (s     "1.12.0"))
+  :url "https://gitlab.com/matsievskiysv/math-preview"
+  :commit "a2ca3c175468ceaf02bab6cdfd8ef016bda2b98d"
+  :revdesc "a2ca3c175468"
+  :keywords '("convenience"))
